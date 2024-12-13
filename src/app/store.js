371b578e11e19814a38044efad075859b8cdd9ca
[Pedro Martins] <[pdromartins11@gmail.com]>
2024-12-13 17:25:35 -0300

    Add post page

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsSlice from './slices/posts/postsSlice'
+import postInfoSlice from './slices/posts/postInfoSlice'
 
 const store = configureStore({
     reducer: {
-        posts: postsSlice
+        posts: postsSlice,
+        postInfo: postInfoSlice
     }
 })
 
