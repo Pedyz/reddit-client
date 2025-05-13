7537bbf14360cbae21aeeb0000f540bdfd628e5a
[Pedro Martins] <[pdromartins11@gmail.com]>
2025-05-12 23:06:36 -0300

    Add posts and infos to Sub Reddit page

--- src/components/Comments/Comment/Comment.jsx
+++ src/components/Comments/Comment/Comment.jsx
@@ -2,6 +2,7 @@ import Style from './Comment.module.css'
 import upArrow from '../../../images/arrow-up.png'
 import downArrow from '../../../images/down-arrow.png'
 import { useNavigate } from 'react-router-dom'
+import purpleIcon from '../../../images/reddit-logo.png'
 
 function Comment({ name, upvotes, text, icon }) {
     const navigate = useNavigate()
@@ -9,7 +10,7 @@ function Comment({ name, upvotes, text, icon }) {
     return (
         <div className={Style.comment}>
             <div className={Style.userInfo}>
-                <img src={icon} onClick={() => navigate(`/user/${name}`)} />
+                <img src={icon || purpleIcon} onClick={() => navigate(`/user/${name}`)} />
                 <h4 onClick={() => navigate(`/user/${name}`)} >{name}</h4>
             </div>
             <p className={Style.mainText}>{text}</p>
